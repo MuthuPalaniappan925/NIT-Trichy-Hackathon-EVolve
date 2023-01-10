6ce346b3ed2e2d551dac11991ed7c7d580600c6e
Muthu Palaniappan <108980831+MuthuPalaniappan925@users.noreply.github.com>
2023-01-10 15:04:12 +0530

    Contains source code files of Login System

---
+++ login/app.py
@@ -0,0 +1,148 @@
+#importing packages
+from flask import Flask,render_template,request,redirect,url_for,session
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+import re
+
+app = Flask(__name__)
+app.secret_key = 'Muthu_shreya_NIT-T_Hackathon'
+#configuration of MYSQL db - connection
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+
+app.config['MYSQL_PASSWORD'] = 'MuthuPandas9'
+
+app.config['MYSQL_DB'] = 'EVMUTHU'
+
+mysql = MySQL(app)
+
+@app.route('/')
+@app.route('/login', methods =['GET', 'POST'])
+
+def login():
+
+    msg = ''
+
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+
+        username = request.form['username']
+
+        password = request.form['password']
+
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+
+        cursor.execute('SELECT * FROM evstations_list WHERE username = % s AND password = % s', (username, password, ))
+
+        account = cursor.fetchone()
+
+        if account:
+
+            session['loggedin'] = True
+
+            session['id'] = account['id']
+
+            session['username'] = account['username']
+
+            msg = 'Logged in successfully !'
+
+            return render_template('index.html', msg = msg)
+
+        else:
+
+            msg = 'Incorrect username / password !'
+
+    return render_template('login.html', msg = msg)
+ 
+
+@app.route('/logout')
+
+def logout():
+
+    session.pop('loggedin', None)
+
+    session.pop('id', None)
+
+    session.pop('username', None)
+
+    return redirect(url_for('login'))
+ 
+
+@app.route('/register', methods =['GET', 'POST'])
+
+def register():
+
+    msg = ''
+
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form :
+
+        username = request.form['username']
+
+        password = request.form['password']
+
+        email = request.form['email']
+
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+
+        cursor.execute('SELECT * FROM evstations_list WHERE username = % s', (username, ))
+
+        account = cursor.fetchone()
+
+        if account:
+
+            msg = 'Account already exists !'
+
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+
+            msg = 'Invalid email address !'
+
+        elif not re.match(r'[A-Za-z0-9]+', username):
+
+            msg = 'Username must contain only characters and numbers !'
+
+        elif not username or not password or not email:
+
+            msg = 'Please fill out the form !'
+
+        else:
+
+            cursor.execute('INSERT INTO evstations_list VALUES (NULL, % s, % s, % s)', (username, password, email, ))
+
+            mysql.connection.commit()
+
+            msg = 'You have successfully registered !'
+
+    elif request.method == 'POST':
+
+        msg = 'Please fill out the form !'
+
+    return render_template('register.html', msg = msg)
+
+@app.route('/ev_submission',methods=['POST','GET'])
+
+def ev_submission():
+    
+    msg= ' '
+
+    if request.method == 'POST' and 'address' in request.form and 'c_type' in request.form and 'Price' in request.form:
+        print("Here-0")
+        address = request.form['address']
+        c_type = request.form['C_Type']
+        price = request.form['Price']
+        print("Here-1")
+        print(address,c_type,price)
+
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+
+        cursor.execute('INSERT INTO muthu_ev VALUES (NULL, % s, % s, % s)', (address, c_type, price, ))
+
+        mysql.connection.commit()
+
+    msg = 'The details submitted succesfully!!!'
+
+        
+
+    return render_template('ev_Submission.html',msg = msg)
+    #print("Hi Hello")
+
+if __name__ == "__main__":
+    app.run(debug=True)
